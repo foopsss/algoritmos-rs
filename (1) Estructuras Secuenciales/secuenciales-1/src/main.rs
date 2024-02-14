105ec5f584f92697015008ce224c9a28719be408
Lucas <46837214+foopsss@users.noreply.github.com>
2024-02-14 20:02:29 -0300

    Cambio de nombre de la librería "funciones"
    Ahora se llama en inglés, "functions". También cambié el nombre de la función "read_value" por "read_number" porque es más representativo del uso, y porque es posible que después añada una para leer caracteres.
    
    También hice que read_number devuelva ahora un entero con signo de 16 bits, entonces tengo un amplio rango de números positivos y negativos para trabajar.

--- (1) Estructuras Secuenciales/secuenciales-1/src/main.rs
+++ (1) Estructuras Secuenciales/secuenciales-1/src/main.rs
@@ -1,11 +1,11 @@
-use funciones;
+use functions;
 
 fn main() {
     const TASA_INF: f32 = 0.04;
     
-    let año_act: u16 = funciones::read_value("Introduzca el año actual");
-    let precio_act: u16 = funciones::read_value("Introduzca el precio actual del artículo");
-    let año_fut: u16 = funciones::read_value("Introduzca un año a futuro");
+    let año_act: i16 = functions::read_number("Introduzca el año actual");
+    let precio_act: i16 = functions::read_number("Introduzca el precio actual del artículo");
+    let año_fut: i16 = functions::read_number("Introduzca un año a futuro");
     
     let precio_fut: f32 = precio_act as f32 * (1 as f32 + TASA_INF).powf(año_fut as f32 - año_act as f32);
     
--- functions/src/lib.rs
+++ functions/src/lib.rs
@@ -1,10 +1,10 @@
 use std::io;
 use std::io::Write;
 
-pub fn read_value(x: &str) -> u16 {
+pub fn read_number(x: &str) -> i16 {
     // Esta función permite leer un valor introducido por el usuario.
     // También permite mostrar un mensaje personalizado.
-    // Devuelve un entero sin signo de 16 bits para emplearla en general.
+    // Devuelve un entero con signo de 16 bits para emplearla en general.
     let mut value = String::new();
 
     print!("{x}: ");
@@ -14,7 +14,7 @@ pub fn read_value(x: &str) -> u16 {
     io::stdin().read_line(&mut value)
         .expect("¡Error!");
 
-    let value: u16 = value.trim().parse()
+    let value: i16 = value.trim().parse()
         .expect("¡Introduzca un número válido!");
 
     value
