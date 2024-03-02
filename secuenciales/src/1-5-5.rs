85a55ab9965a5a697b331817077309c5c2110ad4
Lucas <46837214+foopsss@users.noreply.github.com>
2024-03-02 20:32:03 -0300

    Modificación del ejercicio 1.5.5
    Remoción de los acentos escritos en el nombre de una variable.

--- secuenciales/src/1-5-5.rs
+++ secuenciales/src/1-5-5.rs
@@ -4,8 +4,8 @@ fn main() {
     let corriente: f64 = read_double("Introduzca el valor de la corriente eléctrica del conductor");
     let conductividad: f64 = read_double("Introduzca el valor de la conductividad eléctrica del conductor");
 
-    let sección: f64 = (corriente / conductividad) + (corriente / conductividad) * 0.25;
+    let seccion: f64 = (corriente / conductividad) + (corriente / conductividad) * 0.25;
 
     println!("");
-    println!("La sección del conductor es: {sección}");
+    println!("La sección del conductor es: {seccion}");
 }
