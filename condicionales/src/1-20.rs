294e305d959c8030b492a5bc4454d3860df46727
Lucas <libenchat@hotmail.com>
2024-03-22 15:57:00 -0300

    Adición de ejercicios
    Adición de los ejercicios 1.20 y 1.21.

---
+++ condicionales/src/1-20.rs
@@ -0,0 +1,24 @@
+use functions::read_input;
+
+fn main() {
+    let mut numero = read_input::<i16>("Ingrese un número");
+
+    let mut cuadrado;
+    let mut cubo;
+
+    println!("");
+    for i in 1..=10 {
+        cuadrado = numero.pow(2);
+        cubo = numero.pow(3);
+
+        println!("Número: {numero}");
+        println!("Cuadrado: {cuadrado}");
+        println!("Cubo: {cubo}");
+
+        if i < 10 {
+            println!("");
+        }
+
+        numero = numero + 1;
+    }
+}
---
+++ condicionales/src/1-21.rs
@@ -0,0 +1,26 @@
+use functions::read_input;
+
+fn main() {
+    let mut numero = read_input::<i16>("Ingrese un número");
+    let incremento = read_input::<i16>("Ingrese la cantidad de veces a incrementarlo");
+
+    let mut cuadrado;
+    let mut cubo;
+    let mut suma_cuadrados = 0;
+
+    println!("");
+    for _i in 1..=incremento {
+        cuadrado = numero.pow(2);
+        cubo = numero.pow(3);
+
+        println!("Número: {numero}");
+        println!("Cuadrado: {cuadrado}");
+        println!("Cubo: {cubo}");
+        println!("");
+
+        suma_cuadrados = suma_cuadrados + cuadrado;
+        numero = numero + 1;
+    }
+
+    println!("El resultado de la suma de cuadrados es {suma_cuadrados}.");
+}
