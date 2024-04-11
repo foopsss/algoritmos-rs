62e5c241a3ae251632afedcb1118d7bc6200e47e
Lucas <libenchat@hotmail.com>
2024-04-11 12:26:28 -0300

    Adición del ejercicio 1.37.3

---
+++ subacciones/src/1-37-3.rs
@@ -0,0 +1,34 @@
+use functions::read_input;
+use functions::draw_line;
+
+fn devolver_menor(num_a: i16, num_b: i16, num_c: i16) -> i16 {
+    if num_a < num_b && num_a < num_c {
+        num_a
+    } else if num_b < num_a && num_b < num_c {
+        num_b
+    } else {
+        num_c
+    }
+}
+
+fn main() {
+    let mut eleccion = 'S';
+
+    while eleccion == 'S' {
+        let a = read_input::<i16>("Ingrese un valor numérico para A");
+        let b = read_input::<i16>("Ingrese un valor numérico para B");
+        let c = read_input::<i16>("Ingrese un valor numérico para C");
+
+        let menor = devolver_menor(a, b, c);
+
+        println!("");
+        println!("De los números introducidos, el menor es {}.", menor);
+
+        println!("");
+        eleccion = read_input::<char>("¿Desea realizar otra comparación? [S/N]");
+
+        if eleccion == 'S' {
+            draw_line(45);
+        }
+    }
+}
