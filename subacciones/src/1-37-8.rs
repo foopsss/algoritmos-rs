50d8f3320678c4e6e4dcfe0301aecdb01147b025
Lucas <libenchat@hotmail.com>
2024-05-10 01:18:19 -0300

    Adición del 1-37-8
    Lo hice para probar el modelo hecho en seudocódigo, pero ya que está hecho lo publico.
    
    Esto me ayudo a hacer el ejercicio: https://stackoverflow.com/questions/18479648/does-rust-have-support-for-functions-that-return-multiple-values.

---
+++ subacciones/src/1-37-8.rs
@@ -0,0 +1,27 @@
+use functions::read_input;
+
+fn intercambiar(mut a: i16, mut b: i16) -> (i16, i16) {
+    let resguardo = a;
+    a = b;
+    b = resguardo;
+
+    (a, b)
+}
+
+fn main() {
+    let num_a = read_input::<i16>("Introduzca un valor para A");
+    let num_b = read_input::<i16>("Introduzca un valor para B");
+
+    println!();
+    println!("Primer valor de A: {num_a}");
+    println!("Primer valor de B: {num_b}");
+
+    // "intercambiar" devuelve una tupla, así que
+    // le asigno individualmente a cada variable
+    // los valores retornados por la función.
+    let (num_a, num_b) = intercambiar(num_a, num_b);
+
+    println!();
+    println!("Nuevo valor de A: {num_a}");
+    println!("Nuevo valor de B: {num_b}");
+}
